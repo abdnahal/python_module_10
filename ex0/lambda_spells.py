4bac2abde9952bf42797f9cc52bfe39c24183c81
Abdellatif Nahal <abdnahal@e1r13p12.1337.ma>
2026-03-13 07:05:19 +0000

    ex0

--- ex0/lambda_spells.py
+++ ex0/lambda_spells.py
@@ -5,6 +5,25 @@ def artifact_sorter(artifacts: List[dict]) -> List[dict]:
     return sorted(artifacts, key=lambda x: x['power'], reverse=True)
 
 
+def power_filter(mages: List[dict], min_power: int) -> List[dict]:
+    filtered = list(filter(lambda x: True if x['power'] >= min_power
+                           else False, mages))
+    return filtered
+
+
+def spell_transformer(spells: List[str]) -> List[str]:
+    transformed = list(map(lambda s: '* ' + s + ' *', spells))
+    return transformed
+
+
+def mage_stats(mages: list[dict]) -> dict:
+    _max = max(mages, key=lambda x: x['power'])
+    _min = min(mages, key=lambda x: x['power'])
+    _sum = sum(mages, key=lambda x: x['power'])
+    avg = round(_sum / len(mages), 2)
+    return {"max_power": _max, "min_power": _min, "avg_power": avg}
+
+
 if __name__ == "__main__":
     artifacts = [{'name': 'Fire Staff', 'power': 86, 'type': 'accessory'},
                  {'name': 'Crystal Orb', 'power': 84, 'type': 'weapon'},
